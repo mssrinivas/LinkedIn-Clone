fd2f808d4880f720f713f8b31490e53feb219eb5
M.S.Srinivas <mssrinivas@users.noreply.github.com>
2018-11-15 20:39:36 -0800

    Updated Main.js

--- linkedin-frontend/src/components/main.js
+++ linkedin-frontend/src/components/main.js
@@ -4,12 +4,9 @@ import { Provider } from 'react-redux';
 import store from './store/store.js';
 import JobListing from './job_listings/joblisting.js';
 import JobPostings from './JobPostings/JobPostings'
-<<<<<<< HEAD
 import CustomJobApply from './JobApply/customApply.js';
-=======
 import Login from './applicant/login'
 import {history} from './../util/utils';
->>>>>>> 7f7091c7ca041d9a8afff6291e0be2af8b28756f
 
 class Main extends Component {
     render(){
@@ -19,11 +16,8 @@ class Main extends Component {
             <Switch>
                 <Route exact path="/listings" component={JobListing} />
                 <Route exact path="/postjob" component={JobPostings} />
-<<<<<<< HEAD
                 <Route exact path="/customapply" component={CustomJobApply} />
-=======
                 <Route exact path="/" component={Login} />
->>>>>>> 7f7091c7ca041d9a8afff6291e0be2af8b28756f
             </Switch>
             </Router>
             </Provider>
