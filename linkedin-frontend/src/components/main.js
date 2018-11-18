1d022617c9d8c1398a53aa0cac9edbed5fcb98ad
Shivam Waghela <shivam.waghela@sjsu.edu>
2018-11-17 22:52:17 -0800

    Implemented Navbar

--- linkedin-frontend/src/components/main.js
+++ linkedin-frontend/src/components/main.js
@@ -1,27 +1,29 @@
-import React, {Component} from 'react';
-import {Switch,Route, Router} from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store/store.js';
-import JobListing from './job_listings/joblisting.js';
-import JobPostings from './JobPostings/JobPostings'
-import CustomJobApply from './JobApply/customApply.js';
-import Login from './applicant/login'
-import {history} from './../util/utils';
-class Main extends Component {
-    render(){
-        return(
-            <Provider store={store}>
-            <Router history={history}>
-            <Switch>
-                <Route exact path="/listings" component={JobListing} />
-                <Route exact path="/postjob" component={JobPostings} />
-                <Route exact path="/customapply" component={CustomJobApply} />
-                <Route exact path="/" component={Login} />
-            </Switch>
-            </Router>
-            </Provider>
+import React, { Component } from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import JobListing from "./job_listings/joblisting.js";
+import JobPostings from "./JobPostings/JobPostings";
+import CustomJobApply from "./JobApply/customApply.js";
+import Login from "./applicant/login";
+import { history } from "./../util/utils";
+import Navbar from "./navbar/Navbar";
 
-        )
-    }
+class Main extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/listings" component={JobListing} />
+            <Route exact path="/postjob" component={JobPostings} />
+            <Route exact path="/customapply" component={CustomJobApply} />
+            <Route exact path="/" component={Login} />
+            <Route exact path="/navbar" component={Navbar} />
+          </Switch>
+        </Router>
+      </Provider>
+    );
+  }
 }
 export default Main;
