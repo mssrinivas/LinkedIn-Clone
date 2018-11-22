e69a61513fb477e8dfa6ae09d924b77296ce96aa
Arihant Sai <you@example.com>
2018-11-21 16:33:35 -0800

    added easyApplyModal in job listings

--- linkedin-frontend/src/components/job_listings/joblistcard.js
+++ linkedin-frontend/src/components/job_listings/joblistcard.js
@@ -17,12 +17,12 @@ class JobListCard extends Component {
         return ( 
             <div className="row joblist-row justify-content-center" onClick={this.cardClicked} >
                 <div className="col-md-2">
-                    <img className="w-100 contain" id="logo-company-img" src={data.companyLogo} alt={data.companyname} />
+                    <img className="w-100 contain" id="logo-company-img" src={data.CompanyLogo} alt={data.CompanyName} />
                 </div>
                 <div className="col-md-8">
-                    <p className="position-name" style={{color:'#0073b1',fontSize:'16px'}}> {data.jobTitle} {data.jobFunction} </p>
-                    <p className="company-name" style={{fontSize:'14px',color:'black'}} > {data.companyName} </p>
-                    <p className="location" style={{fontSize:'13px',color:'grey',fontStyle:'bold'}} > {data.location} </p>
+                    <p className="position-name" style={{color:'#0073b1',fontSize:'16px'}}> {data.JobTitle} {data.JobFunction} </p>
+                    <p className="company-name" style={{fontSize:'14px',color:'black'}} > {data.CompanyName} </p>
+                    <p className="location" style={{fontSize:'13px',color:'grey',fontStyle:'bold'}} > {data.JobLocation} </p>
                     <p className="description" style={{fontSize:'14px'}} > {data.description.substring(0, data.description.length>100?100:data.description.length)}... </p>
                     <p className="apply" style={{fontSize:'12px',color:'grey'}} ><b>Posted On: {new Date(data.postingDate).toDateString()}</b></p>
                     <div style={{display:data.easyApply?'block':'none'}}>
